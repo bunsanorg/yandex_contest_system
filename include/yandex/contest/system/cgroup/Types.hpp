d5ea4621bfbf015f36d60697b7c67699b9089f0d
Aleksey Filippov <sarum9in@gmail.com>
2012-09-06 22:14:55 +0400

    cgroup: UnitsConverter support.

--- include/yandex/contest/system/cgroup/Types.hpp
+++ include/yandex/contest/system/cgroup/Types.hpp
@@ -5,4 +5,21 @@
 namespace yandex{namespace contest{namespace system{namespace cgroup
 {
     typedef std::uint64_t Count;
+
+    namespace detail
+    {
+        template <typename Units>
+        struct UnitsConverter
+        {
+            static Count unitsToCount(const Units units)
+            {
+                return static_cast<Count>(units);
+            }
+
+            static Units countToUnits(const Count count)
+            {
+                return static_cast<Units>(count);
+            }
+        };
+    }
 }}}}
