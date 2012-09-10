9d14153a8375f8f7ed3e595e2969ef0c9d44b1a8
Filippov Aleksey <sarum9in@gmail.com>
2012-09-10 10:02:37 +0400

    cgroup::MemoryBase wrapper member functions implementation.
    Functions:
        moveChargeAtImmigrate()
        setMoveChargeAtImmigrate()
    
        softLimitInBytes()
        setSoftLimitInBytes()
    
        oomControl()
        setOomControl()
    
        forceEmpty()

--- include/yandex/contest/system/cgroup/Memory.hpp
+++ include/yandex/contest/system/cgroup/Memory.hpp
@@ -14,6 +14,33 @@ namespace yandex{namespace contest{namespace system{namespace cgroup
     public:
         static const std::string SUBSYSTEM_NAME;
         static const boost::optional<std::string> UNITS;
+
+    public:
+        struct MoveChargeAtImmigrateConfig
+        {
+            MoveChargeAtImmigrateConfig()=default;
+
+            explicit MoveChargeAtImmigrateConfig(const int mask);
+
+            int mask() const;
+
+            bool anonymous = false;
+            bool file = false;
+        };
+
+        MoveChargeAtImmigrateConfig moveChargeAtImmigrate() const;
+        void setMoveChargeAtImmigrate(
+            const MoveChargeAtImmigrateConfig &moveChargeAtImmigrate) const;
+        void setMoveChargeAtImmigrate(
+            const bool anonymous=true, const bool file=true) const;
+
+        Count softLimitInBytes() const;
+        void setSoftLimitInBytes(const Count limit) const;
+
+        bool oomControl() const;
+        void setOomControl(const bool oomControl=true) const;
+
+        void forceEmpty() const;
     };
 
     typedef Subsystem<MemoryBase> Memory;
--- src/cgroup/Memory.cpp
+++ src/cgroup/Memory.cpp
@@ -4,4 +4,56 @@ namespace yandex{namespace contest{namespace system{namespace cgroup
 {
     const std::string MemoryBase::SUBSYSTEM_NAME("memory");
     const boost::optional<std::string> MemoryBase::UNITS("bytes");
+
+    MemoryBase::MoveChargeAtImmigrateConfig::MoveChargeAtImmigrateConfig(const int mask):
+        anonymous(mask & (1 << 0)), file(mask & (1 << 1)) {}
+
+    int MemoryBase::MoveChargeAtImmigrateConfig::mask() const
+    {
+        return (static_cast<int>(anonymous) << 0) | (static_cast<int>(file) << 1);
+    }
+
+    MemoryBase::MoveChargeAtImmigrateConfig MemoryBase::moveChargeAtImmigrate() const
+    {
+        return MoveChargeAtImmigrateConfig(readField<int>("move_charge_at_immigrate"));
+    }
+
+    void MemoryBase::setMoveChargeAtImmigrate(
+        const MoveChargeAtImmigrateConfig &moveChargeAtImmigrate) const
+    {
+        writeField("move_charge_at_immigrate", moveChargeAtImmigrate.mask());
+    }
+
+    void MemoryBase::setMoveChargeAtImmigrate(const bool anonymous, const bool file) const
+    {
+        MoveChargeAtImmigrateConfig cfg;
+        cfg.anonymous = anonymous;
+        cfg.file = file;
+        setMoveChargeAtImmigrate(cfg);
+    }
+
+    Count MemoryBase::softLimitInBytes() const
+    {
+        return readField<Count>("soft_limit_in_bytes");
+    }
+
+    void MemoryBase::setSoftLimitInBytes(const Count limit) const
+    {
+        writeField("soft_limit_in_bytes", limit);
+    }
+
+    bool MemoryBase::oomControl() const
+    {
+        return readField<int>("oom_control");
+    }
+
+    void MemoryBase::setOomControl(const bool oomControl) const
+    {
+        writeField<int>("oom_control", oomControl);
+    }
+
+    void MemoryBase::forceEmpty() const
+    {
+        writeField("force_empty", 0);
+    }
 }}}}
