78bbeb912534c8f0ac6cb1bbbe959ff2031c8a50
Filippov Aleksey <sarum9in@gmail.com>
2012-09-13 00:04:34 +0400

    unistd::DynamicLibrary partial implementation.

---
+++ src/unistd/DynamicLibrary.cpp
@@ -0,0 +1,72 @@
+#include "yandex/contest/system/unistd/DynamicLibrary.hpp"
+
+#include <dlfcn.h>
+
+namespace yandex{namespace contest{namespace system{namespace unistd
+{
+    DynamicLibrary::DynamicLibrary():
+        handle_(nullptr) {}
+
+    DynamicLibrary::DynamicLibrary(const boost::filesystem::path &filename, const int flags):
+        handle_(dlopen(filename.c_str(), flags | RTLD_NOLOAD))
+    {
+        if (handle_)
+            BOOST_THROW_EXCEPTION(DynamicLibraryIsAlreadyResidentError() <<
+                                  DynamicLibraryError::filename(filename) <<
+                                  DynamicLibraryError::flags(flags));
+        handle_ = dlopen(filename.c_str(), flags);
+        if (!handle_)
+            BOOST_THROW_EXCEPTION(DynamicLibraryError() <<
+                                  DynamicLibraryError::filename(filename) <<
+                                  DynamicLibraryError::flags(flags));
+    }
+
+    DynamicLibrary::DynamicLibrary(DynamicLibrary &&dl)
+    {
+        swap(dl);
+        dl.close();
+    }
+
+    DynamicLibrary &DynamicLibrary::operator=(DynamicLibrary &&dl)
+    {
+        swap(dl);
+        dl.close();
+        return *this;
+    }
+
+    void DynamicLibrary::close()
+    {
+        if (handle_)
+        {
+            if (dlclose(handle_))
+            {
+                const char *const err = dlerror();
+                BOOST_ASSERT(err);
+                BOOST_THROW_EXCEPTION(DynamicLibraryError() << DynamicLibraryError::dlerror(err));
+            }
+        }
+    }
+
+    void DynamicLibrary::swap(DynamicLibrary &dl) noexcept
+    {
+        using std::swap;
+        swap(handle_, dl.handle_);
+    }
+
+    DynamicLibrary::operator bool() const noexcept
+    {
+        return handle_;
+    }
+
+    DynamicLibrary::~DynamicLibrary()
+    {
+        try
+        {
+            close();
+        }
+        catch (...)
+        {
+            // does nothing
+        }
+    }
+}}}}
