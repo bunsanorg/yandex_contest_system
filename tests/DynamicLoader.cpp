dcc95bb2262fd89b1e1e76f7fe8e4059b023eeb4
Aleksey Filippov <sarum9in@gmail.com>
2012-09-20 14:35:52 +0400

    unistd::DLInfo operator<<() implementation.

---
+++ tests/DynamicLoader.cpp
@@ -0,0 +1,27 @@
+#define BOOST_TEST_MODULE DynamicLoader
+#include <boost/test/unit_test.hpp>
+
+#include "yandex/contest/system/unistd/DynamicLoader.hpp"
+
+namespace unistd = yandex::contest::system::unistd;
+
+BOOST_AUTO_TEST_SUITE(DynamicLoader)
+
+int function(int a, int b)
+{
+    return a + b;
+}
+
+static int sfunction(int a, int b)
+{
+    return a + b;
+}
+
+BOOST_AUTO_TEST_CASE(dladdr)
+{
+    BOOST_TEST_MESSAGE("test_method: " << unistd::dladdr(&dladdr::test_method));
+    BOOST_TEST_MESSAGE("function: " << unistd::dladdr(&function));
+    BOOST_TEST_MESSAGE("sfunction: " << unistd::dladdr(&sfunction));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
