69659ce859d362c9d4c8f272b9ddd04d3473142c
Aleksey Filippov <sarum9in@gmail.com>
2014-12-29 15:45:53 +0100

    testing::DummyProcess refactoring.

--- tests/DummyProcess.hpp
+++ tests/DummyProcess.hpp
@@ -7,31 +7,20 @@
 
 namespace yandex{namespace contest{namespace system{namespace testing
 {
-    class DummyProcess
+    class DummyProcess: public execution::AsyncProcess
     {
     public:
-        DummyProcess(): process_(options()) {}
-
-        void stop()
-        {
-            process_.stop();
-        }
-
-        execution::AsyncProcess::Pid pid() const
-        {
-            return process_.pid();
-        }
+        DummyProcess(): execution::AsyncProcess(options()) {}
 
     private:
         static execution::AsyncProcess::Options options()
         {
             execution::AsyncProcess::Options opt;
-            opt.executable = bunsan::testing::dir::tests::resources::binary() / "dummy_process";
+            opt.executable =
+                bunsan::testing::dir::tests::resources::binary() /
+                "dummy_process";
             opt.usePath = false;
             return opt;
         }
-
-    private:
-        execution::AsyncProcess process_;
     };
 }}}}
